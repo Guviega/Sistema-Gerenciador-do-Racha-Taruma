ec187b5b219e071c2f03c74551abeef2e68163d8
Gustavo Viega <102606442+Guviega@users.noreply.github.com>
2025-06-06 21:24:38 -0300

    First Implementation
    Implemented first model classes: Veiculo, Piloto, Corrida with initial atributtes

---
+++ src/sistemagerenciadorrachataruma/model/Corrida.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagerenciadorrachataruma.model;
+
+/**
+ *
+ * @author DELL
+ */
+public class Corrida {
+    
+    private Veiculo veiculo1, veiculo2;
+    
+}
---
+++ src/sistemagerenciadorrachataruma/model/Veiculo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagerenciadorrachataruma.model;
+
+/**
+ *
+ * @author DELL
+ */
+public class Veiculo {
+    
+    private TipoVeiculo tipoVeiculo;
+    private String modelo;
+    private Piloto piloto;
+    
+}
