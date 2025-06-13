63bcd3c441c7bc5ddc8af6500eaa69edf16b23f5
Antony <167705338+DevTonyz@users.noreply.github.com>
2025-06-13 20:03:39 -0300

    insert equipe class

---
+++ src/sistemagerenciadorrachataruma/model/Equipe.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagerenciadorrachataruma.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author DELL
+ */
+public class Equipe {
+    
+    private String nome;
+    //private List<Piloto> pilotos;
+}
+
